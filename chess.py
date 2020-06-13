79cbdf2e19525f6bce8ea15e34c1084b6119e4d9
Duke Wright <dhwright25@gmail.com>
2020-06-12 18:55:15 -0700

    Added FPS/stat display

--- chess.py
+++ chess.py
@@ -1,48 +1,49 @@
 import json
-import random
 import pygame
 
 with open('config.json') as f:
     config = json.load(f)
 
-color = {
-    'black': (0, 0, 0),
-    'white': (255, 255, 255),
-    'red': (255, 0, 0),
-    'green': (0, 255, 0),
-    'blue': (0, 0, 255),
-    'grey': (84, 84, 84)
-}
-
-
 def create_board(size, tile_size):
-    board = pygame.Surface((tile_size * size, tile_size * size))
-    board.fill(color['white'])
+    chessboard = pygame.Surface((tile_size * size, tile_size * size))
+    chessboard.fill(pygame.Color("white"))
     tile_count = 0
     for x in range(size):
         for y in range(size):
             if tile_count % 2 == 0:
-                pygame.draw.rect(board, color['black'], (x * tile_size, y * tile_size, tile_size, tile_size))
+                pygame.draw.rect(chessboard, pygame.Color("black"), (x * tile_size, y * tile_size, tile_size, tile_size))
             else:
-                pygame.draw.rect(board, color['white'], (x * tile_size, y * tile_size, tile_size, tile_size))
+                pygame.draw.rect(chessboard, pygame.Color("white"), (x * tile_size, y * tile_size, tile_size, tile_size))
             tile_count += 1
         tile_count -= 1
-    return board
+    return chessboard
+
+
+def update_stats():
+    fps = str(int(clock.get_fps()))
+    fps_text = font.render("FPS: " + str(fps), 1, pygame.Color("red"))
+    return fps_text
+
 
 pygame.init()
 pygame.display.set_caption('Chess Sandbox')
 screen = pygame.display.set_mode((config['screen-width'], config['screen-height']))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 18)
+FPS = config['fps']
 running = True
 
 while running:
+    clock.tick(FPS)
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(color['grey'])  # clear screen
+    screen.fill(pygame.Color("dimgray"))  # clear screen
     board = create_board(config['board-size'], config['tile-size'])
     screen.blit(board, board.get_rect(center=(config['board-center-x'], config['board-center-y'])))
+    screen.blit(update_stats(), (config['stats-x'], config['stats-y']))
     pygame.display.update()
 
 pygame.quit()
