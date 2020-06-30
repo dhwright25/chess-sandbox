7e17e520f7e3edabd5d5dd53be660528532becbb
Duke Wright <dhwright25@gmail.com>
2020-06-29 22:01:21 -0700

    Refactored so engine can be used for multiple games. Basically a customizable wrapper of sorts?

--- engine/engine.py
+++ engine/engine.py
@@ -1,6 +1,7 @@
 import pygame
 import json
 
+
 def load_config(filename):
     with open(filename) as f:
         return json.load(f)
@@ -12,17 +13,22 @@ class Engine:
         self.running = False
         pygame.init()
         self.screen = pygame.display.set_mode((self.config['screen-width'], self.config['screen-height']))
+        pygame.display.set_caption(self.config['title'])
         self.fps = self.config['fps']
         self.font = pygame.font.SysFont(self.config['font'], self.config['font-size'])
-        self.controllers = list()
+        self.show_stats = True
+        self.entities = list()
+
+    def toggle_stats(self):
+        self.show_stats = False if self.show_stats else True
 
     def update_stats(self, clock):
         fps = str(int(clock.get_fps()))
-        fps_text = self.font.render("FPS: " + str(fps), 1, pygame.Color("red"))
+        fps_text = self.font.render("FPS: " + str(fps), 1, pygame.Color(self.config['stats-color']))
         return fps_text
 
-    def add_controller(self, target):
-        self.controllers.append(target)
+    def add_entities(self, *target):
+        self.entities.append(*target)
 
     def run(self):
         self.running = True
@@ -30,24 +36,37 @@ class Engine:
         while self.running:
             dt = clock.tick(self.fps) / 1000.
             events = pygame.event.get()
+            self.key_listener(events)
             self.update(dt, events)
             self.draw(self.screen)
-            print(self.update_stats(clock))
-            #self.screen.blit(self.update_stats(clock), (self.config['stats-x'], self.config['stats-y']))
+            if self.show_stats:
+                self.screen.blit(self.update_stats(clock), (self.config['stats-x'], self.config['stats-y']))
+                pygame.display.update()
         pygame.quit()
 
     def stop(self):
         self.running = False
 
     def update(self, dt, events):
-        for c in self.controllers:
+        for c in self.entities:
             c.update(dt, events)
 
     def draw(self, screen):
-        screen.fill(pygame.Color("dimgray"))
-        for c in self.controllers:
+        screen.fill(pygame.Color(self.config['background-color']))
+        for c in self.entities:
             c.draw(screen)
         pygame.display.flip()
+
+    def key_listener(self, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.stop()
+            if event.type == pygame.KEYDOWN:
+                char = chr(event.key)
+                if char in self.config['key-binds'].keys():
+                    getattr(self, self.config['key-binds'][char])()
+
+
 
 
 
--- game/chess.py
+++ game/chess.py
@@ -1,83 +1,18 @@
-import json
 import pygame
-from game.player import Player
-
-with open('game/config.json') as f:
-    config = json.load(f)
+from engine.engine import Engine
+from game.board import Board
 
 
-def create_board(size, tile_size):
-    chessboard = pygame.Surface((tile_size * size, tile_size * size))
-    chessboard.fill(pygame.Color("white"))
-    tile_count = 0
-    for x in range(size):
-        for y in range(size):
-            if tile_count % 2 == 0:
-                pygame.draw.rect(chessboard, pygame.Color("black"),
-                                 (x * tile_size, y * tile_size, tile_size, tile_size))
-            else:
-                pygame.draw.rect(chessboard, pygame.Color("white"),
-                                 (x * tile_size, y * tile_size, tile_size, tile_size))
-            tile_count += 1
-        tile_count -= 1
-    return chessboard
+class Chess:
+
+    def __init__(self):
+        self.engine = Engine()
+        self.board = Board()
+        self.engine.add_entities(self.board)
+
+    def start_game(self):
+        self.engine.run()
 
 
-def load_pieces(filename, width, height, x_offset=0, y_offset=0):
-    sheet = pygame.image.load(filename)
-    pieces = [{}, {}]
-    for i, rank in enumerate(["r", "n", "b", "q", "k", "p"]):
-        for side in range(2):
-            pieces[side][rank] = sheet.subsurface((i * width) + x_offset, (side * height) + y_offset, width, height)
-    return pieces
-
-
-def test_drawing_pieces(screen, pieces, width, height):
-    piece_list = list(list(side.values()) for side in pieces)
-    pieces = [piece for side in piece_list for piece in side]
-    for i, piece in enumerate(pieces):
-        screen.blit(piece, (i * width, 50, width, height))
-
-
-screen = None
-clock = None
-font = None
-FPS = None
-piece_sprites = None
-
-
-def init():
-    global screen
-    global clock
-    global font
-    global FPS
-    global piece_sprites
-    pygame.init()
-    pygame.display.set_caption('Chess Sandbox')
-    screen = pygame.display.set_mode((config['screen-width'], config['screen-height']))
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("Arial", 18)
-    FPS = config['fps']
-    piece_sprites = load_pieces(config['pieces-filename'], config['piece-width'], config['piece-height'])
-
-
-def loop():
-    play_1 = Player("b")
-    print(play_1)
-    while True:
-        clock.tick(FPS)
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        screen.fill(pygame.Color("dimgray"))  # clear screen
-        board = create_board(config['board-size'], config['tile-size'])
-        screen.blit(board, board.get_rect(center=(config['board-center-x'], config['board-center-y'])))
-        screen.blit(update_stats(), (config['stats-x'], config['stats-y']))
-        test_drawing_pieces(screen, piece_sprites, config['piece-width'], config['piece-height'])
-        pygame.display.update()
-
-
-init()
-loop()
+game = Chess()
+game.start_game()
